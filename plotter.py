432b9b45b56ae1985a86daf78a9b292a8200d635
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-12-23 23:35:30 +0200

    Plot a function

--- plotter.py
+++ plotter.py
@@ -1,17 +1,31 @@
 # Initial window made following https://stackoverflow.com/questions/12459811/how-to-embed-matplotlib-in-pyqt-for-dummies
 
 import sys
+
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLineEdit, QFormLayout, QLabel
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
+import sympy as sym
+from sympy.parsing.sympy_parser import standard_transformations
+from sympy.parsing.sympy_parser import implicit_multiplication_application
+from sympy.parsing.sympy_parser import convert_xor
+
+import numpy as np
+
+def parse_function(text):
+    """parses incoming text and returns a sympy expression"""
+    transformations = (standard_transformations + (convert_xor, implicit_multiplication_application))
+    expression = sym.parse_expr(text, transformations=transformations, evaluate=False)
+    return expression
+
 
 class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
-
 
         # a figure instance to plot on
         self.figure = plt.figure()
@@ -30,8 +44,12 @@ class Window(QDialog):
 
         # text boxes to enter function, min x, and max x
         self.functionLineEdit = QLineEdit(self)
+
         self.minXLineEdit = QLineEdit(self)
+        self.minXLineEdit.setValidator(QIntValidator())
+
         self.maxXLineEdit = QLineEdit(self)
+        self.maxXLineEdit.setValidator(QIntValidator())
 
         # Label to display messages and errors
         self.errorsLabel = QLabel(self)
@@ -50,11 +68,52 @@ class Window(QDialog):
         generalLayout.addWidget(self.errorsLabel)
         self.setLayout(generalLayout)
 
+    def showError(self, errorText):
+        self.errorsLabel.setStyleSheet("color: red")
+        self.errorsLabel.setText(errorText)
+
+    def showSuccess(self, sucessText):
+        self.errorsLabel.setStyleSheet("color: green")
+        self.errorsLabel.setText(sucessText)
+
     def plot(self):
 
-        self.errorsLabel.setText(self.functionLineEdit.text())
+        functionTextContent = self.functionLineEdit.text()
+        minX = self.minXLineEdit.text()
+        maxX = self.maxXLineEdit.text()
 
-        data = [i**2 for i in range(100)]
+        # Make sure user provides function, min x, and max x
+        if not functionTextContent:
+            self.showError("Function textbox can't be empty!")
+            return False
+
+        if not minX:
+            self.showError("Must provide minimum value for x")
+            return False
+
+        if not maxX:
+            self.showError("Must provide maximum value of x")
+            return False
+
+        try:
+            expression = parse_function(functionTextContent)
+        except ValueError:
+            self.showError("Invalid expression for function!")
+            return False
+
+        x = sym.Symbol('x')
+        if (len(expression.free_symbols) != 1) or (x not in expression.free_symbols):
+            self.showError("Expression must contain only one variable and it must be x")
+            return False
+
+        minX = int(minX)
+        maxX = int(maxX)
+
+        print(expression, minX, maxX)
+
+        resolution = 100
+        xVals = np.linspace(minX, maxX, resolution)
+        data = [expression.evalf(subs={x: i}) for i in xVals]
 
         self.figure.clear()
 
@@ -62,10 +121,18 @@ class Window(QDialog):
         ax = self.figure.add_subplot(111)
 
         # plot the data
-        ax.plot(data)
+        ax.plot(xVals, data)
 
         # Refresh canvas
         self.canvas.draw()
+
+        self.showSuccess("Showing plot!")
+
+
+    def fillWithTestData(self):
+        self.functionLineEdit.setText("x + 1")
+        self.minXLineEdit.setText("1")
+        self.maxXLineEdit.setText("4")
 
 
 if __name__ == '__main__':
@@ -79,6 +146,7 @@ if __name__ == '__main__':
     """)
 
     main = Window()
+    main.fillWithTestData()
     main.show()
 
     sys.exit(app.exec_())
